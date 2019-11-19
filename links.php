f5f1960c375abab2619304623d03aa520da25be8
Marnix <33731014+TemplarMarnix@users.noreply.github.com>
2019-11-19 11:47:50 +0100

    Werkomgeving gemaakt
    De werkomgeving (bestanden indeling) is gemaakt en we zijn klaar om van start te gaan.

---
+++ links.php
@@ -0,0 +1,7 @@
+<head>
+    <link href="style.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Courgette|Muli&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/37e2a8fe9a.js" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</head>
