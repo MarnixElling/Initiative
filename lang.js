e0d035681ef15af589dc9523615367d4c903b51f
NielsPerdon <40392944+NielsPerdon@users.noreply.github.com>
2019-12-13 15:30:55 +0100

    hoofdletters

--- lang.js
+++ lang.js
@@ -4,54 +4,54 @@ script type = "text/javascript" >
         'en': {
 
             //header.php
-            'vaardigheden': 'skills',
-            'over': 'about us ',
+            'vaardigheden': 'Skills',
+            'over': 'About us ',
             'partners': 'Partners',
-            'email': 'e-mail',
-            'telefoon': 'phone',
-            'settings': 'settings',
-            'kleuren': 'color Scheme',
-            'business': 'business',
-            'friendly': 'friendly',
+            'email': 'E-mail',
+            'telefoon': 'Phone',
+            'settings': 'Settings',
+            'kleuren': 'Color Scheme',
+            'business': 'Business',
+            'friendly': 'Friendly',
             'warm': 'Warm',
-            'taal': 'language',
-            'slogan': 'because with teamwork you get the furthest!',
-            'start': 'start immediately'
+            'taal': 'Language',
+            'slogan': 'Because with teamwork you get the furthest!',
+            'start': 'Start immediately'
             //home.php
             'zie': 'See more',
             'prof': 'Professionality',
             'kennis': 'The knowledge that we possess to make what you need.',
             'wie': 'Who are we?',
             'team': 'the Team',
-            'Email sturen': 'send email:',
-            'voornaam': 'forname',
-            'achternaam': 'lastname',
-            'herhaal': 'repeat Email',
-            'bericht': 'message',
+            'Email sturen': 'Send email:',
+            'voornaam': 'Forname',
+            'achternaam': 'Lastname',
+            'herhaal': 'Repeat Email',
+            'bericht': 'Message',
             //footer.php
             'vragen': 'Frequently Asked Questions',
             'domein': 'Can I buy a domain through INITIATIVE?',
             'hosten': 'Is it possible to host my website through INITIATIVE?',
             'ergens anders': 'What if I want to host my website somewhere else?',
-            'bekijk': 'view more',
+            'bekijk': 'View more',
             'bellen': 'Id like to call',
             'email sturen': 'I would like to send an email',
             'support email': 'I want to send a support email',
             'partnership': 'I would like to discuss a partnership',
             'zaken doen': 'I would like to do business',
-            'copyright': 'copyright © 2020 INITIATIVE. All rights reserved.'
+            'copyright': 'Copyright © 2020 INITIATIVE. All rights reserved.'
 
 
         },
         'nl': {
 
             //header.php
-            'vaardigheden': 'vaardigheden',
-            'over': 'over ons',
+            'vaardigheden': 'Vaardigheden',
+            'over': 'Over ons',
             'partners': 'Partners',
             'email': 'Email',
             'telefoon': 'Telefoon',
-            'settings': 'instellingen',
+            'settings': 'Instellingen',
             'kleuren': 'Kleuren Schema',
             'business': 'Zakelijk',
             'friendly': 'Vriendelijk',
@@ -81,7 +81,7 @@ script type = "text/javascript" >
             'support email': 'Ik wil een support email sturen',
             'partnership': 'Ik zou een partnership willen bespreken',
             'zaken doen': 'Ik zou graag zaken willen doen',
-            'copyright': 'auteursrechten © 2020 INITIATIVE. Alle rechten voorbehouden.'
+            'copyright': 'Auteursrechten © 2020 INITIATIVE. Alle rechten voorbehouden.'
 
         },
         'de': {
@@ -94,14 +94,14 @@ script type = "text/javascript" >
             'telefoon': 'Telefon',
             'settings': 'Institutionen',
             'kleuren': 'Farbschema',
-            'business': 'sachlich',
-            'friendly': 'freundlich',
+            'business': 'Sachlich',
+            'friendly': 'Freundlich',
             'warm': 'Warm',
-            'taal': 'sprache',
+            'taal': 'Sprache',
             'slogan': 'Denn mit Teamwork kommen Sie am weitesten!',
-            'start': 'starte sofort'
+            'start': 'Starte sofort'
             //home.php
-            'zie': 'mehr sehen',
+            'zie': 'Mehr sehen',
             'prof': 'Professionalität',
             'kennis': 'Das Wissen, das wir besitzen, um das zu machen, was Sie brauchen.',
             'wie': 'Wer wir sind?',
@@ -112,7 +112,7 @@ script type = "text/javascript" >
             'herhaal': 'Email wiederholen',
             'bericht': 'Nachricht',
             //footer.php
-            'vragen': 'häufig gestellte fragen',
+            'vragen': 'Häufig gestellte fragen',
             'domein': 'Kann ich über INITIATIVE eine Domain kaufen?',
             'hosten': 'Ist es möglich, meine Website über INITIATIVE zu hosten?',
             'ergens anders': 'Was ist, wenn ich meine Website woanders hosten möchte?',
